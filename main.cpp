765a9b6589475aec0869886b13428442a1666f91
Steven Lovegrove <stevenlovegrove@gmail.com>
2014-12-16 12:07:58 +0000

    WIP: Introduce Heightmap.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <fstream>
-#include <pangolin/pangolin.h>
+#include <map>
 #include <Eigen/Eigen>
 #include <Eigen/StdVector>
-#include <map>
+#include <Eigen/Geometry>
+#include <pangolin/pangolin.h>
 
 struct GcodeCmd
 {
@@ -13,6 +14,7 @@ struct GcodeCmd
 
 void ConsumeComments(std::istream& is)
 {
+    // TODO: This wont handle multiple nested brackets.
     if(is.good() && is.peek() == '(') {
         is.get();
         while(is.peek() != ')') is.get();
@@ -97,11 +99,15 @@ public:
         const int samples = std::max(2, (int)std::ceil(dist * samples_per_unit));
         for(int s=0; s < samples; ++s) {
             const double lambda = (double)s / (double)samples;
-            const Eigen::Vector3d p = (lambda*p_w + (1-lambda)*end_w);
-//            std::cout << p.transpose() << std::endl;
-            trajectory_w.push_back( p );
+            SamplePosition(lambda*p_w + (1-lambda)*end_w);
         }
         end_w = p_w;
+    }
+
+    void SamplePosition(const Eigen::Vector3d& p_w)
+    {
+        bounds_mm.extend(p_w.head<2>());
+        trajectory_w.push_back( p_w );
     }
 
     double samples_per_unit;
@@ -109,6 +115,8 @@ public:
     // Start and end positions for the spindle
     Eigen::Vector3d start_w;
     Eigen::Vector3d end_w;
+
+    Eigen::AlignedBox2d bounds_mm;
 
     // samples on trajectory, absolute coordinates
     std::vector<Eigen::Vector3d,Eigen::aligned_allocator<Eigen::Vector3d> > trajectory_w;
@@ -134,6 +142,59 @@ GcodeProgram ParseFile(const std::string& filename)
     return prog;
 }
 
+class Heightmap
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    Heightmap(const Eigen::AlignedBox2d& bounds)
+        : res_per_mm(10.0f)
+    {
+        bounds_mm = bounds;
+        bounds_mm.min()[0] -= 100;
+        bounds_mm.min()[1] -= 100;
+        bounds_mm.max()[0] += 100;
+        bounds_mm.max()[1] += 100;
+        Eigen::Vector2i size_pix = (res_per_mm*bounds_mm.sizes()).cast<int>();
+        surface = Eigen::Matrix<float,Eigen::Dynamic,Eigen::Dynamic>::Zero(size_pix[0], size_pix[1]);
+    }
+
+    // Assume V tip for now
+    void Mill(const Eigen::Vector3d& p_w)
+    {
+        // Surface strictly z < max_surface_height_mm
+        const double max_surface_height_mm = 0;
+        const double grad = 1.0;
+        const double max_cut_depth_mm = max_surface_height_mm - p_w[2];
+        const double max_rad_mm = grad * max_cut_depth_mm;
+
+        if(max_rad_mm > 0.0) {
+            const Eigen::Vector2d min_mm = p_w.head<2>().array() - max_rad_mm;
+            const Eigen::Vector2d max_mm = p_w.head<2>().array() + max_rad_mm;
+            const Eigen::AlignedBox2d update_pixf(res_per_mm*min_mm - bounds_mm.min(), res_per_mm*max_mm - bounds_mm.min());
+            const Eigen::AlignedBox2i update_pix = update_pixf.cast<int>();
+
+            // Update height for max_rad patch around p_w.xy
+            const Eigen::Vector2d center_pix = (p_w.head<2>() - bounds_mm.min()) * res_per_mm;
+            const double grad_pix = grad * res_per_mm;
+            Eigen::Vector2i pix;
+            for(pix[0] = update_pix.min()[0]; pix[0] <= update_pix.max()[0]; ++pix[0]) {
+                for(pix[1] = update_pix.min()[1]; pix[1] <= update_pix.max()[1]; ++pix[1]) {
+                    const double rad = (pix.cast<double>() - center_pix).norm();
+                    const double tool_height_mm = p_w[2] + rad * grad_pix;
+                    if(tool_height_mm < surface(pix[1],pix[0]) ) {
+                        surface(pix[1],pix[0]) = tool_height_mm;
+                    }
+                }
+            }
+        }
+    }
+
+    double res_per_mm;
+    Eigen::AlignedBox2d bounds_mm;
+    Eigen::Matrix<float,Eigen::Dynamic,Eigen::Dynamic> surface;
+};
+
 int main( int argc, char** argv )
 {  
     if(argc <= 1) {
@@ -143,6 +204,15 @@ int main( int argc, char** argv )
 
     const std::string filename = argv[1];
     GcodeProgram prog = ParseFile(filename);
+
+    std::cout << prog.bounds_mm.min().transpose() << std::endl;
+    std::cout << prog.bounds_mm.max().transpose() << std::endl;
+
+    Heightmap heightmap(prog.bounds_mm);
+    for(const Eigen::Vector3d& p_w : prog.trajectory_w)
+    {
+        heightmap.Mill(p_w);
+    }
 
     pangolin::CreateWindowAndBind("Main",640,480);
     glEnable(GL_DEPTH_TEST);
